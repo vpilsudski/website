74ae6e7a06ee343bc58c85ed1cd7c72705dc20f4
Cedric Gatay <c.gatay@code-troopers.com>
2018-04-04 10:36:20 +0200

    chore(): initial import

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+
+
+
+
+}); // fin document ready
