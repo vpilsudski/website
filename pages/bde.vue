18a5b26f88127065c17e3bd15a24cc63ede4f067
NicolasDM <40382984+NicolasDM@users.noreply.github.com>
2019-09-03 17:15:47 +0200

    Update bde.vue

--- pages/bde.vue
+++ pages/bde.vue
@@ -282,7 +282,7 @@
         ></div>
         <div class="bloc-carre carre-col-1">
 
-          <h3>Tarifs de<br>Chapitô<br>à partir de 3300€</h3>
+          <h3>Profitez de<br>Chapitô<br>à partir de 3300€</h3>
           <p>Accédez à l’outil Chapitô afin de générez une application pour votre événement sur iPhone et sur Android</p>
         </div>
 
